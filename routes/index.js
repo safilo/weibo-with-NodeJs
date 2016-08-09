8aca64833760c93625656a5151a9a7e2b2893f92
safilo <wulinqing@hotmail.com>
2016-08-09 11:57:03 +0800

    add home page sidebar data

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var express = require('express'),
 
 /* GET home page. */
 router.get('/', middle.checkNotLogin, function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express',sidebar : ['推荐','奥运', '明星', '视频', '搞笑', '情感', '社会', '综艺', '美妆', '美食', '美女模特', '...更多']});
 });
 
 router.get('/:username/home', middle.checkLogin, function(req, res, next) {
