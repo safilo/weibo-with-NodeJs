57f35aaaeefcd41c864e47ed22a6a8ac59f62858
safilo <wulinqing@hotmail.com>
2016-08-09 22:48:34 +0800

    remove debug.log

--- settings.js
+++ settings.js
@@ -2,11 +2,11 @@ module.exports = {
     cookieSecret:'wusafilo',
     db:'wlqing',
     // product
-    host:'123.57.143.189',
+    /*host:'123.57.143.189',
     port:27017,
-    url:"mongodb://123.57.143.189:27017/wlqing"
+    url:'mongodb://123.57.143.189:27017/wlqing'*/
     // develop
-    /*host:'localhost',
+    host:'localhost',
     port:27017,
-    url:"mongodb://localhost:27017/weibo"*/
+    url:'mongodb://localhost:27017/weibo'
 };
